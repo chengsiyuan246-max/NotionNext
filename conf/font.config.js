46677554aa1f1975e15a322a1683b2f9c7afbf53
chengsiyuan246-max <chengsiyuan246@gmail.com>
2026-08-20 21:56:33 +0800

    Add Google Sans font to configuration

--- conf/font.config.js
+++ conf/font.config.js
@@ -10,6 +10,7 @@ module.exports = {
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: process.env.NEXT_PUBLIC_FONT_URL || [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+    'https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap',
     'https://fonts.googleapis.com/css?family=Bitter:300,400,700&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;400;500;700&display=swap'
@@ -22,6 +23,7 @@ module.exports = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
+    '"Google Sans"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
